a99f24cdfe20f427c070fb3e4e8500495cb30698
Enz <hello@ecalamia.com>
2021-12-13 19:10:36 +0100

    Add smileys

--- Containerfile
+++ Containerfile
@@ -3,5 +3,6 @@ FROM python:3.9-slim-bullseye
 COPY requirements.txt /tmp/
 RUN pip install -U pip
 RUN pip install --no-cache-dir -r /tmp/requirements.txt
-ADD bot bot
+ADD bot/bot.py bot/bot.py
+ADD bot/sm.py bot/sm.py
 ENTRYPOINT ["python", "bot/bot.py"]
--- bot/bot.py
+++ bot/bot.py
@@ -1,11 +1,28 @@
 import os
 import sys
+import random
 import discord
 import aioredis
 
 import sm
 
 PREFIX = 'sm_bot'
+EMOJIS = [
+    ':smirk:',
+    ':thinking:',
+    ':upside_down:',
+    ':stuck_out_tongue:',
+    ':tux:',
+    ':metal:',
+    ':love_letter:',
+    ':drooling_face:',
+    ':kissing_smiling_eyes:',
+    ':kissing_heart:',
+    ':heart_eyes:',
+    ':star_struck:',
+    ':money_mouth:',
+    ':money_with_wings:',
+]
 
 client = discord.Client()
 redis = aioredis.from_url(os.getenv('REDIS_URL', 'redis://localhost:6379/0'))
@@ -22,27 +39,39 @@ async def on_message(message):
     if message.author == client.user or message.author.bot:
         return
 
-    if client.user in message.mentions:
+    if client.user not in message.mentions:
+        return
 
-        if '!ping' in message.content:
-            await message.channel.send('pong')
-            return
+    if '!ping' in message.content:
+        await message.channel.send('pong')
+        return
 
-        for _ in range(10):  # Try 10 times before giving up
-            async with redis.pipeline() as pipe:
-                s1, s2 = await (pipe.srandmember(f'{PREFIX}:author1')
-                                    .srandmember(f'{PREFIX}:author2')
-                                    .execute())
-            sentence = sm.make_sentence(s1.decode('utf-8'), s2.decode('utf-8'))
-            if sentence is not None:
-                break
+    if '!stats' in message.content:
+        async with redis.pipeline() as pipe:
+            c1, c2 = await (pipe.scard(f'{PREFIX}:author1')
+                                .scard(f'{PREFIX}:author2')
+                                .execute())
+        await message.channel.send(
+            f'Author 1: {c1} sentences. Author 2: {c2} sentences.'
+        )
+        return
 
-        # If we are unlucky and haven't found any combination that
-        # makes a sentence, then we fall back to the default message.
-        if sentence is None:
-            sentence = "Je suis à court d'idées :smirk:"
+    for _ in range(10):  # Try 10 times before giving up
+        async with redis.pipeline() as pipe:
+            s1, s2 = await (pipe.srandmember(f'{PREFIX}:author1')
+                                .srandmember(f'{PREFIX}:author2')
+                                .execute())
+        sentence = sm.make_sentence(s1.decode('utf-8'), s2.decode('utf-8'))
+        if sentence is not None:
+            break
 
-        await message.reply(sentence)
+    # If we are unlucky and haven't found any combination that
+    # makes a sentence, then we fall back to the default message.
+    if sentence is None:
+        sentence = "Je suis à court d'idées"
+
+    emoji = random.choice(EMOJIS)
+    await message.reply(sentence + ' ' + emoji)
 
 
 if __name__ == '__main__':
